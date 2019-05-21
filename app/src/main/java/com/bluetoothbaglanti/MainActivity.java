f65b79597528029b5f2cf68d07b0c87ecdd7e2bb
Burcu Söylemez <burcu.soylemez@bil.omu.edu.tr>
2019-05-21 16:47:49 +0300

    Create design

---
+++ app/src/main/java/com/bluetoothbaglanti/MainActivity.java
@@ -0,0 +1,22 @@
+package com.bluetoothbaglanti;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Switch onOff;
+    TextView showStatu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        onOff = findViewById(R.id.swButton);
+        showStatu = findViewById(R.id.statuText);
+    }
+}
